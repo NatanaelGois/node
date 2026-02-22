8489c5a4e5f7af02043d8ea3ccebc537d741e427
NatanaelGois01 <natanael.gois@dcomp.ufs.br>
2026-02-22 18:00:50 -0300

    NovoModulo

---
+++ funcao.js
@@ -0,0 +1,16 @@
+function mult(a, b){
+    return a*b;
+}
+
+let x = mult(5,2);
+
+console.log(x);
+
+function toCelsius(f){
+    return (5/9) * (f - 32);
+}
+
+let temp = toCelsius(77);
+let texto = "A temperatura é "+ temp +" Celsius ";
+
+console.log(texto);
