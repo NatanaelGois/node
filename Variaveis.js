a9cb186d7643666a75355bc33c6a2e23115dd1a7
NatanaelGois01 <natanael.gois@dcomp.ufs.br>
2026-02-22 17:43:14 -0300

    Teste de Variáveis

---
+++ Variaveis.js
@@ -0,0 +1,21 @@
+var nome = "João";
+let nome1 = "Maria"
+const nome2 = "Karla"
+
+
+function func1(){
+    var sobrenome ="Silva";
+    console.log("Olá " + nome +" "+ sobrenome);
+    console.log("Olá ",+nome1);
+}
+
+function func2(){
+    console.log("Oi "+ nome +" ");]
+
+}
+
+func1();
+func2();
+
+console.log(nome);
+console.log(nome1);
